b1429fec03c7a8c98ae94a190855caa26bcd0b57
cesarucal <cesar.garcia1@ucalgary.ca>
2024-02-12 12:35:21 -0700

    changes

--- Lab03/ex3.py
+++ Lab03/ex3.py
@@ -1,11 +1,14 @@
+import matplotlib.pyplot as plt
+from random import randint
+
 
 #Bubble sort that counts comparisons and swaps
 def bubble_sort(arr):
-	swaps = 0
-	comparisons = 0
 	n = len(arr)
 	
 	for i in range(n):
+		global swaps
+		global comparisons
 		for j in range(0, n-i-1):
 			comparisons += 1
 			if arr[j] > arr[j+1]:
@@ -13,4 +16,63 @@ def bubble_sort(arr):
 				arr[j] = arr[j+1]
 				arr[j+1] = temp
 				swaps += 1
-	return swaps, comparisons
+	return arr
+
+nums = [1000, 2000, 4000, 8000, 16000, 32000]
+comp = []
+swap = []
+
+arr1000 = [randint(1, 1000) for i in range(1000)]
+
+arr2000 = [randint(1, 2000) for i in range(2000)]
+
+arr4000 = [randint(1, 4000) for i in range(4000)]
+
+arr8000 = [randint(1, 8000) for i in range(8000)]
+
+swaps = 0
+comparisons = 0
+
+bubble_sort(arr1000)
+
+comp.append(comparisons)
+swap.append(swaps)
+
+comparisons = 0
+swaps = 0
+
+bubble_sort(arr2000)
+
+comp.append(comparisons)
+swap.append(swaps)
+
+comparisons = 0
+swaps = 0
+
+bubble_sort(arr4000)
+
+comp.append(comparisons)
+swap.append(swaps)
+
+comparisons = 0
+swaps = 0
+
+bubble_sort(arr8000)
+
+comp.append(comparisons)
+swap.append(swaps)
+
+comparisons = 0
+swaps = 0
+
+
+plt.plot(nums, comp, label='Comparisons')
+plt.plot(nums, swap, label='Swaps')
+
+plt.xlabel('Array Size')
+plt.ylabel('Count')
+plt.title('Bubble Sort Performance')
+
+plt.legend()
+
+plt.show()
